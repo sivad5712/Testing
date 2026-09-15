82de0378d60e18ae04770defcddaccb0a9d773fe
sivad5712 <sivad5712@gmail.com>
2026-09-15 12:22:58 -0400

    Update addition test to use new values

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -11,7 +11,7 @@ class CalculatorTest {
 
     @Test
     void testAddition() {
-        assertEquals(15, calculator.add(10, 5));
+        assertEquals(30, calculator.add(20, 10));
     }
 
     @Test
